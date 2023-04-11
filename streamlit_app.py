c8650e1684692fcf2efcbc14b4b24ed8feaff44d
alikon <optimus4joomla@gmail.com>
2023-04-11 11:19:23 +0200

    frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,7 +136,7 @@ genderList = gender_classes
 ageNet = age_net
 ageList = ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 
-video=cv2.VideoCapture(frame if frame else 0)
+video=cv2.VideoCapture(frame)
 hasFrame,frame=video.read()
 if not hasFrame:
    st.write('error')
