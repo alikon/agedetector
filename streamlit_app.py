7e0623f385bf7f388a9868dfa469840988806483
alikon <optimus4joomla@gmail.com>
2023-04-11 20:49:46 +0200

    conf

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,10 +156,10 @@ for faceBox in faceBoxes:
     genderNet.setInput(blob)
     genderPreds=genderNet.forward()
     gender=genderList[genderPreds[0].argmax()]
-    st.write(f'Gender: {gender}')
+    st.write(f'Gender: {gender} confidence= {genderPreds[0].max()}')
     ageNet.setInput(blob)
     agePreds=ageNet.forward()
     age=ageList[agePreds[0].argmax()]
-    st.write(f'Age: {age[1:-1]} years, confidence= {agePreds.max()}')
+    st.write(f'Age: {age[1:-1]} years, confidence= {agePreds[0].max()}')
     cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
 #    cv2.imshow("Detecting age and gender", resultImg)
