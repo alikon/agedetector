bbe4c29ed8966f5507fe922d2d365beb1066d848
alikon <optimus4joomla@gmail.com>
2023-04-11 10:34:47 +0200

    redo

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,67 +52,58 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
 else:
     image = Image.open(DEMO_IMAGE)
-    cap = np.array(image)
-    cv2.imwrite('temp.jpg', cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY))
-    cap=cv2.imread('temp.jpg')
 
-    face_txt_path="opencv_face_detector.pbtxt"
-    face_model_path="opencv_face_detector_uint8.pb"
-
-    age_txt_path="age_deploy.prototxt"
-    age_model_path="age_net.caffemodel"
-
-    gender_txt_path="gender_deploy.prototxt"
-    gender_model_path="gender_net.caffemodel"
-
-    MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-    age_classes=['Age: ~1-2', 'Age: ~3-5', 'Age: ~6-14', 'Age: ~16-22',
-                   'Age: ~25-30', 'Age: ~32-40', 'Age: ~45-50', 'Age: age is greater than 60']
-    gender_classes = ['Male', 'Female']
-
-    age_net = cv2.dnn.readNet(age_model_path, age_txt_path)
-    gender_net = cv2.dnn.readNet(gender_model_path, gender_txt_path)
-    face_net = cv2.dnn.readNet(face_model_path, face_txt_path)
-
-    padding = 20
-    t = time.time()
-    frameFace, b_boxes = get_face_box(face_net, cap)
-    if not b_boxes:
-        st.write("No face Detected, Checking next frame")
-
-    for bbox in b_boxes:
-        face = cap[max(0, bbox[1] -
-                       padding): min(bbox[3] +
-                                    padding, cap.shape[0] -
-                                    1), max(0, bbox[0] -
-                                            padding): min(bbox[2] +
-                                                          padding, cap.shape[1] -
-                                                          1)]
-
-        blob = cv2.dnn.blobFromImage(
-            face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-        gender_net.setInput(blob)
-        gender_pred_list = gender_net.forward()
-        gender = gender_classes[gender_pred_list[0].argmax()]
-        st.write(
-            f"Gender : {gender}, confidence = {gender_pred_list[0].max() * 100}%")
-
-        age_net.setInput(blob)
-        age_pred_list = age_net.forward()
-        age = age_classes[age_pred_list[0].argmax()]
-        st.write(f"Age : {age}, confidence = {age_pred_list[0].max() * 100}%")
-
-        label = "{},{}".format(gender, age)
-        cv2.putText(
-            frameFace,
-            label,
-            (bbox[0],
-             bbox[1] - 10),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.8,
-            (0,
-             255,
-             255),
-            2,
-            cv2.LINE_AA)
-        st.image(frameFace)
+cap = np.array(image)
+cv2.imwrite('temp.jpg', cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY))
+cap=cv2.imread('temp.jpg')
+face_txt_path="opencv_face_detector.pbtxt"
+face_model_path="opencv_face_detector_uint8.pb"
+age_txt_path="age_deploy.prototxt"
+age_model_path="age_net.caffemodel"
+gender_txt_path="gender_deploy.prototxt"
+gender_model_path="gender_net.caffemodel"
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+age_classes=['Age: ~1-2', 'Age: ~3-5', 'Age: ~6-14', 'Age: ~16-22',
+               'Age: ~25-30', 'Age: ~32-40', 'Age: ~45-50', 'Age: age is greater than 60']
+gender_classes = ['Male', 'Female']
+age_net = cv2.dnn.readNet(age_model_path, age_txt_path)
+gender_net = cv2.dnn.readNet(gender_model_path, gender_txt_path)
+face_net = cv2.dnn.readNet(face_model_path, face_txt_path)
+padding = 20
+t = time.time()
+frameFace, b_boxes = get_face_box(face_net, cap)
+if not b_boxes:
+    st.write("No face Detected, Checking next frame")
+for bbox in b_boxes:
+    face = cap[max(0, bbox[1] -
+                   padding): min(bbox[3] +
+                                padding, cap.shape[0] -
+                                1), max(0, bbox[0] -
+                                        padding): min(bbox[2] +
+                                                      padding, cap.shape[1] -
+                                                      1)]
+    blob = cv2.dnn.blobFromImage(
+        face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+    gender_net.setInput(blob)
+    gender_pred_list = gender_net.forward()
+    gender = gender_classes[gender_pred_list[0].argmax()]
+    st.write(
+        f"Gender : {gender}, confidence = {gender_pred_list[0].max() * 100}%")
+    age_net.setInput(blob)
+    age_pred_list = age_net.forward()
+    age = age_classes[age_pred_list[0].argmax()]
+    st.write(f"Age : {age}, confidence = {age_pred_list[0].max() * 100}%")
+    label = "{},{}".format(gender, age)
+    cv2.putText(
+        frameFace,
+        label,
+        (bbox[0],
+         bbox[1] - 10),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        (0,
+         255,
+         255),
+        2,
+        cv2.LINE_AA)
+    st.image(frameFace)
